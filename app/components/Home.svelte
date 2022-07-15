f53a3383692154db75a3e22da9cabbdef43e0b88
Ewa Kadziolka <kalistaewa@gmail.com>
2022-07-15 04:19:30 +0200

    first commit

---
+++ app/components/Home.svelte
@@ -0,0 +1,54 @@
+<page class="page">
+	<actionBar title="quote of the day" class="action-bar">
+	</actionBar>
+	<stackLayout class="home">
+        <image  src = "~/images/img{imgNumber}.png"/>
+        <textView text={quote} class="quote"/>
+        <textView text={author} class="quote"/>
+		<button text="Next quote" on:tap="{ onTap }" class="button" />
+	</stackLayout>
+</page>
+
+<script>
+	//import { alert } from '@nativescript/core/ui/dialogs'
+    import { quotes } from '../quotes.js'
+    
+    let min = 1
+    let max = 10
+    let imgNumber
+    let quote
+    let author 
+
+    function randomImage() {
+	imgNumber = Math.floor(Math.random() * (max - min)) + min; 
+    }
+
+    function randomQuote(){
+        let i = Math.floor(Math.random() * quotes.length)
+        quote = quotes[i].quote
+        author = quotes[i].author
+        return quote, author
+    }
+    randomQuote()
+
+    function randomItems(){
+        randomImage();
+        randomQuote();
+    }
+
+   $: imgNumber ? 1 : randomImage();
+
+
+	const onTap = () => randomItems() ;
+</script>
+
+<style>
+    .home{
+        background-color: #3aff96;
+    }
+    .quote{
+        color: blue;
+        font-size: 20rem;
+        font-weight: 600;
+    }
+</style>
---
+++ app/quotes.js
@@ -0,0 +1,92 @@
+   export let quotes = [
+    {
+        "id":1,
+        "author": "Marcus Aurelius",
+        "quote": "Waste no more time arguing what a good man should be. Be One."
+    },
+    {
+        "id":2,
+        "author": "Marcus Aurelius",
+        "quote": "You could leave life right now. Let that determine what you do and say and think."
+    },
+    {
+        "id":3,
+        "author": "Seneca",
+        "quote": "He who fears death will never do anything worth of a man who is alive."
+    },
+    {
+        "id":4,
+        "author": "Seneca",
+        "quote": "Life is very short and anxious for those who forget the past, neglect the present, and fear the future."
+    },
+    {
+        "id":5,
+        "author": "Epictetus",
+        "quote": "How long are you going to wait before you demand the best for yourself?"
+    },
+    {
+        "id":6,
+        "author": "Epictetus",
+        "quote": "Don’t explain your philosophy. Embody it."
+    },
+    {
+        "id":7,
+        "author": "Marcus Aurelius",
+        "quote": "You have power over your mind — not outside events. Realize this, and you will find strength."
+    },
+    {
+        "id":8,
+        "author": "Marcus Aurelius",
+        "quote": "Stop drifting…Sprint to the finish. Write off your hopes, and if your well-being matters to you, be your own savior while you can."
+    },
+    {
+        "id":9,
+        "author": "Seneca",
+        "quote": "Hang on to your youthful enthusiasms — you’ll be able to use them better when you’re older."
+    },
+    {
+        "id":10,
+        "author": "Epictetus",
+        "quote": "Wealth consists not in having great possessions, but in having few wants."
+    },
+    {
+        "id":11,
+        "author": "Epictetus",
+        "quote": "If you want to improve, be content to be thought foolish and stupid."
+    },
+    {
+        "id":12,
+        "author": "Marcus Aurelius",
+        "quote": "If it is not right, do not do it; if it is not true, do not say it."
+    },
+    {
+        "id":13,
+        "author": "Seneca",
+        "quote": "Whatever can happen at any time can happen today."
+    },
+    {
+        "id":14,
+        "author": "Seneca",
+        "quote": "They lose the day in expectation of the night, and the night in fear of the dawn."
+    },
+    {
+        "id":15,
+        "author": "Seneca",
+        "quote": "Begin at once to live, and count each separate day as a separate life."
+    },
+    {
+        "id":16,
+        "author": "Epictetus",
+        "quote": "The key is to keep company only with people who uplift you, whose presence calls forth your best."
+    },
+    {
+        "id":17,
+        "author": "Marcus Aurelius",
+        "quote": "The happiness of your life depends upon the quality of your thoughts."
+    },
+    {
+        "id":18,
+        "author": "Seneca",
+        "quote": "Luck is what happens when preparation meets opportunity."
+    }
+]
